1eeadaeaabaeb3d3e0810d7f9caa906d24976924
Nastena <freziay@mail.ru>
2023-01-05 21:55:53 +0300

    lambdaWorker

---
+++ Main.java
@@ -0,0 +1,12 @@
+package lambdaWorker;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Worker.OnTaskDoneListener listener = System.out::println;
+        Worker.OnTaskErrorListener listenerError = System.out::println;
+        Worker worker = new Worker(listener,listenerError);
+        worker.start();
+    }
+}
---
+++ Worker.java
@@ -0,0 +1,31 @@
+package lambdaWorker;
+
+public class Worker {
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i==33){
+                errorCallback.onError("Error " + i);
+            } else {
+                callback.onDone("Task " + i + " is done");
+            }
+        }
+    }
+
+    @FunctionalInterface
+    public interface OnTaskDoneListener {
+        void onDone(String result);
+    }
+    @FunctionalInterface
+    public interface OnTaskErrorListener {
+        void onError(String result);
+    }
+
+    public Worker(OnTaskDoneListener callback,OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+
+    }
+}
